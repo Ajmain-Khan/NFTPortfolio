8f259064a06fa14dc3b12c4419e09e1cc366ade6
Ajmain Khan <ark.ajmain@gmail.com>
2021-06-02 17:41:29 -0400

    Add project files.

---
+++ NFTPortfolio/NFTPortfolio/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NFTPortfolio.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NFTPortfolio.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}

ed293ff99f396dd6b96d2a7ae86c63a098d40f48
Ajmain Khan <ark.ajmain@gmail.com>
2021-06-02 18:10:23 -0400

    Flyout App Empty Template

--- NFTPortfolio/NFTPortfolio/App.xaml.cs
+++ NFTPortfolio/NFTPortfolio/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using NFTPortfolio.Services;
-using NFTPortfolio.Views;
-using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace NFTPortfolio
 {
@@ -13,7 +9,6 @@ namespace NFTPortfolio
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
